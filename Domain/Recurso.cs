11ac45b3b1e2ba1816ba103e994fb0b1525b7bfa
Milagros Berneda Pereira-Castro <bernedamili@gmail.com>
2025-04-19 17:57:11 -0300

    se agrego atributo en recurso

--- Domain/Recurso.cs
+++ Domain/Recurso.cs
@@ -5,4 +5,5 @@ public class Recurso
     public string nombre { get; set; }
     public string tipo { get; set; }
     public string descripcion { get; set; }
+    public string estado { get; set; }
 }
